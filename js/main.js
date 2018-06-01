2f9fb8d9a007f36aa5adae5284ea16ba07035397
Catink123 <qwerty.qaz120@gmail.com>
2018-06-01 10:53:26 +0300

    download fix

--- js/main.js
+++ js/main.js
@@ -9,6 +9,5 @@ if (document.addEventListener) {
     }
 
 function downloadClk () {
-    alert('Download cliked!!!');
     window.location.href = 'https://1drv.ms/u/s!AsqGVIF15c0wj0Y5NLIWlc7j8Vq0';
 }

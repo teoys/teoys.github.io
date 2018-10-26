a7e15fc3c8d3bc7118d7fc6dfc3900d54ec113dc
catink123 <qwerty.qaz120@gmail.com>
2018-10-26 08:14:48 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+function link (path) {
+    $(".container").fadeOut(300, function () {
+        $(".container").load(path + " .blurb", function () {
+            $(".container").fadeIn(300);
+        });
+    });
+}
